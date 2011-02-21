7400d8e100417b2aef7291e732969881800aa7cc
goshawk <vincenzo.ampolo@gmail.com>
2011-02-21 16:33:14 +0100

    uwsgi

--- cacerp/callcenter/admin.py
+++
@@ -1,7 +0,0 @@
-from models import *
-from django.contrib import admin
-
-admin.site.register(Persona)
-admin.site.register(Lettera)
-admin.site.register(Chiamata)
-admin.site.register(Visita)
--- cacerp/data/indirizzi.sql
+++
@@ -1,8 +0,0 @@
-LOAD DATA LOCAL INFILE '/home/goshawk/Projects/cacerp/cacerp/data/gallicano2010.csv'
-INTO TABLE callcenter_persona
-FIELDS TERMINATED BY ','
-ENCLOSED BY '"'
-ESCAPED BY '"'
-LINES TERMINATED BY '\n'
-(cognome, nome, indirizzo, numero_civico, cap, citta, provincia, telefono);
-
