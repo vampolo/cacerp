57aef2008ae4cb6e783d46b0cfc7cfc32b16a54c
goshawk <vincenzo.ampolo@gmail.com>
2011-03-28 19:17:24 +0200

    new indirizzi

--- data/indirizzi.sql
+++ data/indirizzi.sql
@@ -1,8 +1,8 @@
-LOAD DATA LOCAL INFILE '/home/goshawk/Projects/cacerp/cacerp/data/gallicano2010.csv'
+LOAD DATA LOCAL INFILE 'indirizzi-gallicano-marzo-2011.csv'
 INTO TABLE callcenter_persona
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 ESCAPED BY '"'
 LINES TERMINATED BY '\n'
-(cognome, nome, indirizzo, numero_civico, cap, citta, provincia, telefono);
+(cognome, nome, indirizzo, cap, citta, provincia, telefono);
 
